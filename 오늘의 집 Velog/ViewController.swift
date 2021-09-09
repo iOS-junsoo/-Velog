38f64c653fd017795a8eff964550c0bfa55cc0d7
kimjunsoo <rlawnstn01023@naver.com>
2021-09-09 16:11:09 +0900

    Initial Commit

---
+++ 오늘의 집 Velog/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  오늘의 집 Velog
+//
+//  Created by 준수김 on 2021/09/09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
